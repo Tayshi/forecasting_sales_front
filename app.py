901e5556ae2707fc3111b55f9aa8fe4cd9db8979
Jonathan Nhouyvanisvong <jonathann.pro@yahoo.com>
2022-04-14 17:12:08 +0200

    clean some lines code to present v1

--- app.py
+++ app.py
@@ -65,22 +65,19 @@ def get_predict():
     return sales_fare
 
 sales_fare = get_predict()
-'''Here a prediction'''
 # -----------
 # Request API
 # -----------
 
-# top 10
-# sales_fare['predicted_sales-per_item']['data']
+# sales_fare['predicted_sales-per_item']['data'] # top 10
 
-sales_fare
-
+# sales_fare
 predicted_sales_per_item = json.loads(sales_fare['predicted_sales-per_item'])
-# st.write(predicted_sales_per_item)
+# predicted_sales_per_item
 confidence_int = json.loads(sales_fare['confidence_int']) # confidence_int
-# st.write(confidence_int)
+# confidence_int
 family_forecast = json.loads(sales_fare['family_forecast']) # forecast
-# st.write(family_forecast)
+# family_forecast
 
 
 # Generate DataFrame
@@ -226,38 +223,40 @@ with col_left:
 
 with col_right:
     '### Top 10 des ventes (Sur 1 famille)'
-    # add date to choose
-    sb_year_top_10 = st.selectbox('Year',
-                            range(min(df['date'].dt.year),
-                                max(df['date'].dt.year)))
+    # # add date to choose
+    # sb_year_top_10 = st.selectbox('Year',
+    #                         range(min(df['date'].dt.year),
+    #                             max(df['date'].dt.year)))
 
-    # Later to select meat, chicken, beef, etc.
-    # st.multiselect(label="Sélectionner vos produits", options=df['item_nbr'].columns.tolist(), default=["alcohol","malic_acid"])
+    # # Later to select meat, chicken, beef, etc.
+    # # st.multiselect(label="Sélectionner vos produits", options=df['item_nbr'].columns.tolist(), default=["alcohol","malic_acid"])
 
-    def show_top_10(df, sb_year_top_10):
-        df_in_date = df[df['date'].dt.year == sb_year_top_10]
-        df_top_10 = df_in_date[['family', 'family_sales']].groupby(by='family')\
-                            .sum()\
-                            .sort_values('family_sales', ascending=False).head(10)
-        return df_top_10
+    # def show_top_10(df, sb_year_top_10):
+    #     df_in_date = df[df['date'].dt.year == sb_year_top_10]
+    #     df_top_10 = df_in_date[['family', 'family_sales']].groupby(by='family')\
+    #                         .sum()\
+    #                         .sort_values('family_sales', ascending=False).head(10)
+    #     return df_top_10
 
 
-    st.dataframe(show_top_10(df, sb_year_top_10))
+    # st.dataframe(show_top_10(df, sb_year_top_10))
 
-    def barplot_top10(df):
-        # fig = px.bar(df, x='family_sales', y=df.index, color='item_nbr')
-        fig = px.bar(df, x='family_sales', y=df.index)
+    # def barplot_top10(df):
+    #     # fig = px.bar(df, x='family_sales', y=df.index, color='item_nbr')
+    #     fig = px.bar(df, x='family_sales', y=df.index)
 
-        fig.update_layout(paper_bgcolor='#2C2E43')
-        return fig
+    #     fig.update_layout(paper_bgcolor='#2C2E43')
+    #     return fig
 
-    st.plotly_chart(barplot_top10(show_top_10(df, sb_year_top_10)))
+    # st.plotly_chart(barplot_top10(show_top_10(df, sb_year_top_10)))
+
+
 
     # predicted_sales_per_item['data'] # item_nbr, forecast_product
     df_pred_sales_per_item = pd.DataFrame(predicted_sales_per_item['data'],
                                           columns =['item_nbr', 'forecast_product'])
 
-    st.dataframe(df_pred_sales_per_item)
+    # st.dataframe(df_pred_sales_per_item)
 
     df_pred_sales_per_item['item_nbr'] = df_pred_sales_per_item['item_nbr'].astype(str)
 
@@ -275,9 +274,6 @@ with col_right:
 
     '### Prévisions des ventes'
     # Plot with confidence interval - start
-
-    'Un graphe avec une intervalle de confiance'
-
     # forecast, std_err, confidence_int = {'predicted_sales': '{"columns":["item_nbr","forecast_product"],"index":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],"data":[[103665,118.1805681175],[153239,12.0317315362],[153395,45.4006592073],[153398,119.9685781917],[165718,63.6514990944],[215370,111.586895726],[253103,0.0],[265279,71.2477012761],[269084,59.2145852272],[302952,67.2229645917],[310644,634.6395670119],[310647,46.7342347702],[311994,874.0568623078],[312113,71.9110566686],[315473,0.0],[315474,111.4320607333],[359913,124.2214658545],[360313,98.6543577846],[360314,217.5643155294],[402299,0.0]]}', 'confidence_int': {'6602.960957236907': 15542.673115061367, '5626.846602575789': 14566.55876040025, '6348.378908161257': 15288.091065985718, '6250.10062934992': 15189.812787174382, '5641.551794978621': 14581.26395280308, '7109.3697105270085': 16049.08186835147, '6463.915887184812': 15403.62804500927, '1248.0240571079748': 10187.736214932436, '4936.72242802496': 13876.43458584942, '5176.752296871691': 14116.46445469607, '6850.997021333551': 15790.70917915793, '5552.547742131246': 14492.259899955625, '5399.764680278498': 15199.011466229273, '5381.514230917039': 15180.761016867813, '5753.044452900291': 15552.291238851065, '5366.474603953162': 15165.721389903936, '4633.559635709262': 14432.806421660036, '5797.710271910317': 15596.957057861091, '5840.027843573505': 15639.27462952428, '3276.965667413988': 13076.212453364762}, 'family_predictions': '{"columns":[0],"index":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],"data":[[11072.8170361491],[10096.702681488],[10818.2349870735],[10719.9567082622],[10111.4078738909],[11579.2257894392],[10933.771966097],[5717.8801360202],[9406.5785069372],[9646.6083757839],[11320.8531002457],[10022.4038210434],[10299.3880732539],[10281.1376238924],[10652.6678458757],[10266.0979969285],[9533.1830286846],[10697.3336648857],[10739.6512365489],[8176.5890603894]]}'}
 
 
@@ -325,7 +321,7 @@ with col_right:
 
     image = Image.open('forecasting_sales_front/data/image2.png')
 
-    st.image(image, caption='Prévisions des ventes - Graph')
+    st.image(image, caption='Prévisions des ventes - Graphe')
 
     # Plot with confidence interval
     # plot_forecast(forecast, train_store_1, test_store_1, confidence_int[:,0], confidence_int[:,1])
