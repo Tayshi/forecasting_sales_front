dc855ec5e265b1a76842cb2f4308bfa51c71886c
Jonathan Nhouyvanisvong <jonathann.pro@yahoo.com>
2022-04-14 21:04:22 +0200

    modify some plot and dashboard

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Import
+from inspect import stack
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -74,6 +75,7 @@ sales_fare = get_predict()
 # sales_fare
 predicted_sales_per_item = json.loads(sales_fare['predicted_sales-per_item'])
 # predicted_sales_per_item
+
 confidence_int = json.loads(sales_fare['confidence_int']) # confidence_int
 # confidence_int
 family_forecast = json.loads(sales_fare['family_forecast']) # forecast
@@ -92,7 +94,7 @@ df = get_cached_data()
 df['date'] = pd.to_datetime(df['date'])
 
 '''
-# Prévisions des ventes
+# Prévisions des ventes pour l'année 2016
 ---
 '''
 
@@ -126,7 +128,8 @@ col1.write('## Gestion d\'inventaires')
 col2.write('## Gestion des ventes')
 
 
-with col2:
+col_left, col_right = st.columns(2)
+with col_right:
     date_unit = st.date_input('Chosir la période', datetime.date(2015, 8, 1))
 
     sb_month_unit = date_unit.month
@@ -146,45 +149,78 @@ with col2:
     # # Inventory Units (With + or -) %
     # st.metric("Inventory Units", "121.10", "0.46%")
 
-
-
-col_left, col_right = st.columns(2)
-
-# expander_need_stock = col_left.expander(label='Produits nécessaires à restock')
-# expander_avail_stock = col_left.expander(label='Stocks disponibles (Sur 1 famille)')
-# expander_top_ten = col_right.expander(label='Top 10 des ventes (Sur 1 famille)')
-# expander_predict_sales = col_right.expander(label='Prévisions des ventes')
-
 with col_left:
     '### Inventaire restant en jours'
 
+    # For forecast
+    df_gr_2 = pd.read_csv('raw_data/preprocessed_sales_grouped_2.csv')
+    df_proportion = pd.read_csv('raw_data/proportion_bread.csv', usecols=['item_sales'])
+
+
+    # predicted_sales_per_item['data']
+    forecast_product_20 = np.array([row[1] for row in predicted_sales_per_item['data']]).reshape(20, 1)
+    df_proportion_last_20 = df_proportion.tail(20).values
+
+
+    df_stock = pd.DataFrame(np.hstack((forecast_product_20, df_proportion_last_20)), columns=['initial_stock', 'pred'])
+
+    # np.array(forecast_product_20).reshape(len(forecast_product_20), 1)
+    # df_stock = pd.DataFrame(
+    #     {'initial_stock': df_proportion_last_20,
+    #      'pred': forecast_product_20
+    #      }, index=[range(20)])
+
+
+    df_stock['current_stock'] = df_stock['initial_stock'] - df_stock['pred']
+    df_stock['stock_pred'] = df_stock['initial_stock'] + df_stock['pred']
+
+    # def barplot_matplot_bread(df):
+    #     fig, ax = plt.subplots()
+
+    #     ax.plot(df, kind='bar', stacked=True)
+    #     return fig
+
+    # fig, ax = plt.subplots()
+
+    # st.pyplot(barplot_matplot_bread(df_stock.loc[:,['pred', 'current_stock']]))
+
+    df_stock.loc[:,['pred', 'current_stock']].plot(kind='bar', stacked=True)
+
+    # st.pyplot(fig)
+
+
+    # st.dataframe(df_stock)
+    # st.dataframe(df_stock.loc[:,['pred', 'current_stock']])
+
+
+    # st.bar_chart(df_stock.loc[:,['current_stock', 'initial_stock']])
+    # st.bar_chart(df_stock.loc[:,['current_stock', 'pred']])
+    st.bar_chart(df_stock.loc[:,['current_stock']], height=600)
+
+    # plt.plot(df_stock.loc[:,['pred', 'current_stock']], kind='bar', stack)
+
+    # ------------------------------
+
+
     # value : number of days left (with stock actual )
+
     # bread_barkery_dict = {
-    #     'Pain': 2,
-    #     'Pain de mie': 8,
-    #     'Croissant': 10,
-    #     'Muffin': 8,
-    #     'Tarte aux pommes': 5,
-    #     'Tarte aux chocolats': 6,
-    #     'Sablé aux chocolats': 7,
-    #     'Bretzel': 3
+    #     'Pain de mie': [8, 'Bon'],
+    #     'Pain': [2, 'Alerte'],
+    #     'Croissant': [10, 'Bon'],
+    #     'Muffin': [8, 'Bon'],
+    #     'Tarte aux pommes': [5, 'Alerte'],
+    #     'Tarte aux chocolats': [6, 'Alerte'],
+    #     'Sablé aux chocolats': [7, 'Bon'],
+    #     'Bretzel': [3, 'Alerte']
     #     }
 
-    bread_barkery_dict = {
-        'Pain de mie': [8, 'Bon'],
-        'Pain': [2, 'Alerte'],
-        'Croissant': [10, 'Bon'],
-        'Muffin': [8, 'Bon'],
-        'Tarte aux pommes': [5, 'Alerte'],
-        'Tarte aux chocolats': [6, 'Alerte'],
-        'Sablé aux chocolats': [7, 'Bon'],
-        'Bretzel': [3, 'Alerte']
-        }
+    # df_bread_barkery = pd.DataFrame.from_dict(bread_barkery_dict, orient='index', columns=['nb_days_left', 'alert'])
 
-    df_bread_barkery = pd.DataFrame.from_dict(bread_barkery_dict, orient='index', columns=['nb_days_left', 'alert'])
+
     # st.dataframe(df_bread_barkery)
     def barplot_bread(df_bread):
-        fig = px.bar(df_bread, x='nb_days_left', y=df_bread.index, color='alert', labels={'nb_days_left':'Nombres de jours restants',
+        fig = px.bar(df_bread, x='current_stock', y=df_bread.index, color='alert', labels={'nb_days_left':'Nombres de jours restants',
                                                                            'index': ''})
 
         fig.update_layout(paper_bgcolor='#2C2E43',
@@ -194,42 +230,8 @@ with col_left:
                           showlegend=False)
         return fig
 
-    st.plotly_chart(barplot_bread(df_bread_barkery))
-
-
-    # color not functionnal
-    # def _color_red_or_green(val):
-    #     color = 'red' if val < 10 else 'green'
-    #     return 'color: %s' % color
-
-    # Select store - see after
-    # option = st.selectbox('Select a line to filter', df['store_nbr'].unique())
-    # df_store = df[df['store_nbr'] == option]
-
-
-    # df_store = df
-
-    # df_store = df_store[['family', 'family_sales']]\
-    #                     .groupby(by='family').sum()\
-    #                     .sort_values('family_sales', ascending=False)
-    # df_store['alert'] = (df_store['family_sales'] <= 0)
-
-    # add colors
-    # df_store.style.apply(_color_red_or_green,
-    #                      subset='family_sales', axis=1)
-    # By store
-    # st.dataframe(df_store[['alert', 'family_sales']])
-
-    # too weight
-    def stackbarplot(df):
-        # fig = px.bar(df, y="family", x="family_sales", color='item_nbr' ,title="Prod à restock")
-        fig = px.bar(df, y="family", x="family_sales",title="Produits à restock")
-
-        fig.update_layout(paper_bgcolor='#2C2E43', font_size=24, plot_bgcolor='#2C2E43')
-        return fig
-
-    # st.plotly_chart(stackbarplot(df))
-
+    # Plot Inventory
+    # st.plotly_chart(barplot_bread(df_bread_barkery))
 
     if st.button('Commander'):
         st.write('Commande effectué')
@@ -239,15 +241,15 @@ with col_left:
 
 
 
-    '### Stocks disponibles (Sur 1 famille)'
+    # '### Stocks disponibles (Sur 1 famille)'
 
-    '''
+    # '''
 
-    'Here, a selecbox of family'
+    # 'Here, a selecbox of family'
 
-    Graphe des stocks disponibles
-    Need item_nbr if check 1 family...
-    '''
+    # Graphe des stocks disponibles
+    # Need item_nbr if check 1 family...
+    # '''
     # sb_family_stock = st.selectbox('Family', df['family'].unique())
 
     # df_family = df[df['family'] == sb_family_stock]
@@ -256,7 +258,71 @@ with col_left:
 
 
 with col_right:
-    '### Top 10 des ventes (Sur 1 famille)'
+    '### Prévisions des ventes (Boulangerie/Pâtisserie)'
+    # Plot with confidence interval - start
+    # forecast, std_err, confidence_int = {'predicted_sales': '{"columns":["item_nbr","forecast_product"],"index":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],"data":[[103665,118.1805681175],[153239,12.0317315362],[153395,45.4006592073],[153398,119.9685781917],[165718,63.6514990944],[215370,111.586895726],[253103,0.0],[265279,71.2477012761],[269084,59.2145852272],[302952,67.2229645917],[310644,634.6395670119],[310647,46.7342347702],[311994,874.0568623078],[312113,71.9110566686],[315473,0.0],[315474,111.4320607333],[359913,124.2214658545],[360313,98.6543577846],[360314,217.5643155294],[402299,0.0]]}', 'confidence_int': {'6602.960957236907': 15542.673115061367, '5626.846602575789': 14566.55876040025, '6348.378908161257': 15288.091065985718, '6250.10062934992': 15189.812787174382, '5641.551794978621': 14581.26395280308, '7109.3697105270085': 16049.08186835147, '6463.915887184812': 15403.62804500927, '1248.0240571079748': 10187.736214932436, '4936.72242802496': 13876.43458584942, '5176.752296871691': 14116.46445469607, '6850.997021333551': 15790.70917915793, '5552.547742131246': 14492.259899955625, '5399.764680278498': 15199.011466229273, '5381.514230917039': 15180.761016867813, '5753.044452900291': 15552.291238851065, '5366.474603953162': 15165.721389903936, '4633.559635709262': 14432.806421660036, '5797.710271910317': 15596.957057861091, '5840.027843573505': 15639.27462952428, '3276.965667413988': 13076.212453364762}, 'family_predictions': '{"columns":[0],"index":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],"data":[[11072.8170361491],[10096.702681488],[10818.2349870735],[10719.9567082622],[10111.4078738909],[11579.2257894392],[10933.771966097],[5717.8801360202],[9406.5785069372],[9646.6083757839],[11320.8531002457],[10022.4038210434],[10299.3880732539],[10281.1376238924],[10652.6678458757],[10266.0979969285],[9533.1830286846],[10697.3336648857],[10739.6512365489],[8176.5890603894]]}'}
+
+    train_for_plot_forecast = pd.read_csv('forecasting_sales_front/data/train_14042022.csv')
+    var_forecast = pd.read_csv('forecasting_sales_front/data/forecast.csv')
+
+
+    # Code copied in lesson
+    def plot_forecast(fc, train, test, upper=None, lower=None):
+        is_confidence_int = isinstance(upper, np.ndarray) and isinstance(lower, np.ndarray)
+        # Prepare plot series
+        fc_series = pd.Series(fc, index=test.index)
+        lower_series = pd.Series(upper, index=test.index) if is_confidence_int else None
+        upper_series = pd.Series(lower, index=test.index) if is_confidence_int else None
+
+        # Plot
+        plt.figure(figsize=(10,4), dpi=100)
+        plt.plot(train, label='training', color='black')
+        plt.plot(test, label='reel', color='black', ls='--')
+        plt.plot(fc_series, label='forecast', color='orange')
+        if is_confidence_int:
+            plt.fill_between(lower_series.index, lower_series, upper_series, color='k', alpha=.15)
+        plt.title('Prédictions vs Réel')
+        plt.legend(loc='upper left', fontsize=8);
+
+    # # create df_store_1 for test function predict for BREAD/BAKERY
+    # df_store_1 = pd.read_csv('forecasting_sales_front/data/preprocessed_sales_grouped_1.csv')
+    # df_store_1 = df_store_1[df_store_1['family'] == 'BREAD/BAKERY']
+    # df_store_1['date'] = pd.to_datetime(df_store_1['date'])
+
+    # # test df about BREAD/BAKERY
+
+    # # Prepare train and test
+    # mask_train = (df_store_1['date'] >= '2015-09-01') & (df_store_1['date'] <= '2015-12-31')
+
+    # mask_test = (df_store_1['date'] >= '2016-01-01') & (df_store_1['date'] <= '2016-01-20')
+
+
+    # train_store_1 = df_store_1.loc[df_store_1['family'] == 'BREAD/BAKERY']\
+    #                             .loc[mask_train] # 2013 -> 2015
+
+
+    # test_store_1 = df_store_1.loc[df_store_1['family'] == 'BREAD/BAKERY']\
+    #                             .loc[mask_test] # 2016 ->
+
+    # Plot with confidence interval
+    # plot_forecast(forecast, train_store_1, test_store_1, confidence_int[:,0], confidence_int[:,1])
+    # var_plot_forecast = plot_forecast(family_forecast,
+    #                 train_for_plot_forecast['family_sales'], # x0_BREAD_BAKERY
+    #                 test_for['family_sales'],
+    #                 [row[0] for row in confidence_int],
+    #                 [row[1] for row in confidence_int])
+
+
+    # st.pyplot(var_plot_forecast)
+
+    # image
+    image = Image.open('forecasting_sales_front/data/image2.png')
+
+    st.image(image, caption='Prévisions des ventes - Graphe')
+
+
+
+    '### Top 10 des ventes (Boulangerie/Pâtisserie)'
 
     # predicted_sales_per_item['data'] # item_nbr, forecast_product
     df_pred_sales_per_item = pd.DataFrame(predicted_sales_per_item['data'],
@@ -279,76 +345,15 @@ with col_right:
     st.plotly_chart(barplot_top10(df_pred_sales_per_item))
 
 
-    '### Prévisions des ventes'
-    # Plot with confidence interval - start
-    # forecast, std_err, confidence_int = {'predicted_sales': '{"columns":["item_nbr","forecast_product"],"index":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],"data":[[103665,118.1805681175],[153239,12.0317315362],[153395,45.4006592073],[153398,119.9685781917],[165718,63.6514990944],[215370,111.586895726],[253103,0.0],[265279,71.2477012761],[269084,59.2145852272],[302952,67.2229645917],[310644,634.6395670119],[310647,46.7342347702],[311994,874.0568623078],[312113,71.9110566686],[315473,0.0],[315474,111.4320607333],[359913,124.2214658545],[360313,98.6543577846],[360314,217.5643155294],[402299,0.0]]}', 'confidence_int': {'6602.960957236907': 15542.673115061367, '5626.846602575789': 14566.55876040025, '6348.378908161257': 15288.091065985718, '6250.10062934992': 15189.812787174382, '5641.551794978621': 14581.26395280308, '7109.3697105270085': 16049.08186835147, '6463.915887184812': 15403.62804500927, '1248.0240571079748': 10187.736214932436, '4936.72242802496': 13876.43458584942, '5176.752296871691': 14116.46445469607, '6850.997021333551': 15790.70917915793, '5552.547742131246': 14492.259899955625, '5399.764680278498': 15199.011466229273, '5381.514230917039': 15180.761016867813, '5753.044452900291': 15552.291238851065, '5366.474603953162': 15165.721389903936, '4633.559635709262': 14432.806421660036, '5797.710271910317': 15596.957057861091, '5840.027843573505': 15639.27462952428, '3276.965667413988': 13076.212453364762}, 'family_predictions': '{"columns":[0],"index":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],"data":[[11072.8170361491],[10096.702681488],[10818.2349870735],[10719.9567082622],[10111.4078738909],[11579.2257894392],[10933.771966097],[5717.8801360202],[9406.5785069372],[9646.6083757839],[11320.8531002457],[10022.4038210434],[10299.3880732539],[10281.1376238924],[10652.6678458757],[10266.0979969285],[9533.1830286846],[10697.3336648857],[10739.6512365489],[8176.5890603894]]}'}
-
-
-    # Code copied in lesson
-    def plot_forecast(fc, train, test, upper=None, lower=None):
-        is_confidence_int = isinstance(upper, np.ndarray) and isinstance(lower, np.ndarray)
-        # Prepare plot series
-        fc_series = pd.Series(fc, index=test.index)
-        lower_series = pd.Series(upper, index=test.index) if is_confidence_int else None
-        upper_series = pd.Series(lower, index=test.index) if is_confidence_int else None
-
-        # Plot
-        plt.figure(figsize=(10,4), dpi=100)
-        plt.plot(train, label='training', color='black')
-        plt.plot(test, label='reel', color='black', ls='--')
-        plt.plot(fc_series, label='forecast', color='orange')
-        if is_confidence_int:
-            plt.fill_between(lower_series.index, lower_series, upper_series, color='k', alpha=.15)
-        plt.title('Prédictions vs Réel')
-        plt.legend(loc='upper left', fontsize=8);
-
-    # create df_store_1 for test function predict for BREAD/BAKERY
-    df_store_1 = pd.read_csv('forecasting_sales_front/data/preprocessed_sales_grouped_1.csv')
-    df_store_1 = df_store_1[df_store_1['family'] == 'BREAD/BAKERY']
-    df_store_1['date'] = pd.to_datetime(df_store_1['date'])
-
-    # test df about BREAD/BAKERY
-
-    # Prepare train and test
-    mask_train = (df_store_1['date'] >= '2015-09-01') & (df_store_1['date'] <= '2015-12-31')
-
-    mask_test = (df_store_1['date'] >= '2016-01-01') & (df_store_1['date'] <= '2016-01-20')
-
-
-    train_store_1 = df_store_1.loc[df_store_1['family'] == 'BREAD/BAKERY']\
-                                .loc[mask_train] # 2013 -> 2015
-
-    # breakpoint()
-
-    test_store_1 = df_store_1.loc[df_store_1['family'] == 'BREAD/BAKERY']\
-                                .loc[mask_test] # 2016 ->
-
-    # confidence_int[:,0]
-    # confidence_int[:,1]
-
-    image = Image.open('forecasting_sales_front/data/image2.png')
-
-    st.image(image, caption='Prévisions des ventes - Graphe')
-
-    # Plot with confidence interval
-    # plot_forecast(forecast, train_store_1, test_store_1, confidence_int[:,0], confidence_int[:,1])
-    var_plot_forecast = plot_forecast(family_forecast,
-                    train_store_1['family_sales'], # x0_BREAD_BAKERY
-                    test_store_1['family_sales'],
-                    [row[0] for row in confidence_int],
-                    [row[1] for row in confidence_int])
-
-
-# st.pyplot(var_plot_forecast)
 
 
 
-expander_df = st.expander(label='DataFrame')
-with expander_df:
+# expander_df = st.expander(label='DataFrame')
+# with expander_df:
 
-    # df = df[df['date'].dt.year == 2017]
-    option_head = st.slider('head : ', 1, 1000, 5)
-    st.write(df.head(option_head))
+#     # df = df[df['date'].dt.year == 2017]
+#     option_head = st.slider('head : ', 1, 1000, 5)
+#     st.write(df.head(option_head))
 
-st.write('Min date: ', min(df['date']))
-st.write('Max date: ', max(df['date']))
+# st.write('Min date: ', min(df['date']))
+# st.write('Max date: ', max(df['date']))
